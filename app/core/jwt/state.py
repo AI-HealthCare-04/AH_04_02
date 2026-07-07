5ddc5cb9cac16cbacf38e0f93770da0d6f5f5855
pecs0310 <babi0310@naver.com>
2026-07-07 16:46:36 +0900

    chore: 1주차 MySQL/Docker 템플릿 및 루트 오설치 흔적 정리 (schedule_v6로 대체됨)

--- app/core/jwt/state.py
+++
@@ -1,8 +0,0 @@
-from app.core import config
-from app.core.jwt.backends import TokenBackend
-
-token_backend = TokenBackend(
-    algorithm=config.JWT_ALGORITHM,
-    signing_key=config.SECRET_KEY,
-    leeway=config.JWT_LEEWAY,
-)
